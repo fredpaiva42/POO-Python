d6e7c45cf96c2ab0db4e88db78c1b8d52a4b3841
Frederico Gomes de Paiva <fredericogdpaiva@gmail.com>
2021-09-29 20:33:52 -0300

    Aprendendo sobre encapsulamento

--- account.py
+++ account.py
@@ -2,16 +2,21 @@ class Account:
 
     def __init__(self, number, holder, balance, limit):
         print("Construindo objeto... {}".format(self))
-        self.number = number
-        self.holder = holder
-        self.balance = balance
-        self.limit = limit
+        self.__number = number
+        self.__holder = holder
+        self.__balance = balance
+        self.__limit = limit
 
     def statement(self):
-        print("Saldo de R${} do titular {}.".format(self.balance, self.holder))
+        print("Saldo de R${} do titular {}.".format(
+            self.__balance, self.__holder))
 
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
 
     def deposit(self, amount):
-        self.balance += amount
+        self.__balance += amount
+
+    def transfer(self, amount, destination):
+        self.withdraw(amount)
+        destination.deposit(amount)
